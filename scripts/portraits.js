14648de5e470b7cddb7bb3ad38d32db4597e382c
Ruler90 <54283970+Ruler90@users.noreply.github.com>
2019-08-24 13:28:12 +0200

    Update portraits.js

--- scripts/portraits.js
+++ scripts/portraits.js
@@ -2,33 +2,34 @@
 /* eslint-disable semi */
 const portraitImages = document.querySelectorAll('.portraits');
 const emptyPortrait = document.querySelector('.emptyPortrait');
+const time = +getComputedStyle(document.querySelector('.animation--time')).animationDuration.replace('s', '') * 1000;
 
 function nextPortrait () {
   for (let i = 0; i < portraitImages.length; i++) {
     if (!emptyPortrait.classList.contains('portraitHidden')) {
-      emptyPortrait.classList.add('animation__slideFromZeroToLeft');
-      setTimeout(function () { emptyPortrait.classList.add('portraitHidden') }, 400);
+      emptyPortrait.classList.add('animation', 'animation--time', 'animation__slideFromZeroToLeft');
+      setTimeout(function () { emptyPortrait.classList.add('portraitHidden') }, time);
       portraitImages[0].classList.remove('portraitHidden');
-      portraitImages[0].classList.add('animation__slideFromRightToLeft');
-      setTimeout(function () { portraitImages[0].classList.remove('animation__slideFromRightToLeft') }, 400);
+      portraitImages[0].classList.add('animation', 'animation--time', 'animation__slideFromRightToLeft');
+      setTimeout(function () { portraitImages[0].classList.remove('animation', 'animation--time', 'animation__slideFromRightToLeft') }, time);
       break;
     };
     if (!portraitImages[portraitImages.length - 1].classList.contains('portraitHidden')) {
-      setTimeout(function () { portraitImages[portraitImages.length - 1].classList.add('portraitHidden') }, 400);
-      portraitImages[portraitImages.length - 1].classList.add('animation__slideFromZeroToLeft');
-      setTimeout(function () { portraitImages[portraitImages.length - 1].classList.remove('animation__slideFromZeroToLeft') }, 400);
+      setTimeout(function () { portraitImages[portraitImages.length - 1].classList.add('portraitHidden') }, time);
+      portraitImages[portraitImages.length - 1].classList.add('animation', 'animation--time', 'animation__slideFromZeroToLeft');
+      setTimeout(function () { portraitImages[portraitImages.length - 1].classList.remove('animation', 'animation--time', 'animation__slideFromZeroToLeft') }, time);
       portraitImages[0].classList.remove('portraitHidden');
-      portraitImages[0].classList.add('animation__slideFromRightToLeft');
-      setTimeout(function () { portraitImages[0].classList.remove('animation__slideFromRightToLeft') }, 400);
+      portraitImages[0].classList.add('animation', 'animation--time', 'animation__slideFromRightToLeft');
+      setTimeout(function () { portraitImages[0].classList.remove('animation', 'animation--time', 'animation__slideFromRightToLeft') }, time);
       break;
     }
     if (!portraitImages[i].classList.contains('portraitHidden')) {
-      setTimeout(function () { portraitImages[i].classList.add('portraitHidden') }, 400);
-      portraitImages[i].classList.add('animation__slideFromZeroToLeft');
-      setTimeout(function () { portraitImages[i].classList.remove('animation__slideFromZeroToLeft') }, 400);
+      setTimeout(function () { portraitImages[i].classList.add('portraitHidden') }, time);
+      portraitImages[i].classList.add('animation', 'animation--time', 'animation__slideFromZeroToLeft');
+      setTimeout(function () { portraitImages[i].classList.remove('animation', 'animation--time', 'animation__slideFromZeroToLeft') }, time);
       portraitImages[i + 1].classList.remove('portraitHidden');
-      portraitImages[i + 1].classList.add('animation__slideFromRightToLeft');
-      setTimeout(function () { portraitImages[i + 1].classList.remove('animation__slideFromRightToLeft') }, 400);
+      portraitImages[i + 1].classList.add('animation', 'animation--time', 'animation__slideFromRightToLeft');
+      setTimeout(function () { portraitImages[i + 1].classList.remove('animation', 'animation--time', 'animation__slideFromRightToLeft') }, time);
       break;
     }
   }
@@ -37,29 +38,29 @@ function nextPortrait () {
 function previousPortrait () {
   for (let i = 0; i < portraitImages.length; i++) {
     if (!emptyPortrait.classList.contains('portraitHidden')) {
-      emptyPortrait.classList.add('animation__slideFromZeroToRight');
-      setTimeout(function () { emptyPortrait.classList.add('portraitHidden') }, 400);
+      emptyPortrait.classList.add('animation', 'animation--time', 'animation__slideFromZeroToRight');
+      setTimeout(function () { emptyPortrait.classList.add('portraitHidden') }, time);
       portraitImages[portraitImages.length - 1].classList.remove('portraitHidden');
-      portraitImages[portraitImages.length - 1].classList.add('animation__slideFromLeftToRight');
-      setTimeout(function () { portraitImages[portraitImages.length - 1].classList.remove('animation__slideFromLeftToRight') }, 400);
+      portraitImages[portraitImages.length - 1].classList.add('animation', 'animation--time', 'animation__slideFromLeftToRight');
+      setTimeout(function () { portraitImages[portraitImages.length - 1].classList.remove('animation', 'animation--time', 'animation__slideFromLeftToRight') }, time);
       break;
     };
     if (!portraitImages[0].classList.contains('portraitHidden')) {
-      setTimeout(function () { portraitImages[0].classList.add('portraitHidden') }, 400);
-      portraitImages[0].classList.add('animation__slideFromZeroToRight');
-      setTimeout(function () { portraitImages[0].classList.remove('animation__slideFromZeroToRight') }, 400);
+      setTimeout(function () { portraitImages[0].classList.add('portraitHidden') }, time);
+      portraitImages[0].classList.add('animation', 'animation--time', 'animation__slideFromZeroToRight');
+      setTimeout(function () { portraitImages[0].classList.remove('animation', 'animation--time', 'animation__slideFromZeroToRight') }, time);
       portraitImages[portraitImages.length - 1].classList.remove('portraitHidden');
-      portraitImages[portraitImages.length - 1].classList.add('animation__slideFromLeftToRight');
-      setTimeout(function () { portraitImages[portraitImages.length - 1].classList.remove('animation__slideFromLeftToRight') }, 400);
+      portraitImages[portraitImages.length - 1].classList.add('animation', 'animation--time', 'animation__slideFromLeftToRight');
+      setTimeout(function () { portraitImages[portraitImages.length - 1].classList.remove('animation', 'animation--time', 'animation__slideFromLeftToRight') }, time);
       break;
     }
     if (!portraitImages[i].classList.contains('portraitHidden')) {
-      setTimeout(function () { portraitImages[i].classList.add('portraitHidden') }, 400);
-      portraitImages[i].classList.add('animation__slideFromZeroToRight');
-      setTimeout(function () { portraitImages[i].classList.remove('animation__slideFromZeroToRight') }, 400)
+      setTimeout(function () { portraitImages[i].classList.add('portraitHidden') }, time);
+      portraitImages[i].classList.add('animation', 'animation--time', 'animation__slideFromZeroToRight');
+      setTimeout(function () { portraitImages[i].classList.remove('animation', 'animation--time', 'animation__slideFromZeroToRight') }, time)
       portraitImages[i - 1].classList.remove('portraitHidden');
-      portraitImages[i - 1].classList.add('animation__slideFromLeftToRight');
-      setTimeout(function () { portraitImages[i - 1].classList.remove('animation__slideFromLeftToRight') }, 400);
+      portraitImages[i - 1].classList.add('animation', 'animation--time', 'animation__slideFromLeftToRight');
+      setTimeout(function () { portraitImages[i - 1].classList.remove('animation', 'animation--time', 'animation__slideFromLeftToRight') }, time);
       break;
     }
   }
